b194b795695a19feeebfa56a4a2e5f32d83c0844
king <3925@winnermedical.com>
2022-04-11 19:22:59 +0800

    修复报错

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -6,8 +6,9 @@ var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer {
-    let config = try MirrorConfigration()
-    WeiXinWebHooks.sendContent("服务器已经停止，请马上重启！", app, config)
+    if let config = try? MirrorConfigration() {
+        WeiXinWebHooks.sendContent("服务器已经停止，请马上重启！", app, config)
+    }
     app.shutdown()
 }
 try configure(app)
