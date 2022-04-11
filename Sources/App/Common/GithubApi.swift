3d5e2f27a64aff64482bd9b64747d56754febaef
king <josercc@163.com>
2022-04-11 23:26:56 +0800

    修改请求数量为一个

--- Sources/App/Common/GithubApi.swift
+++ Sources/App/Common/GithubApi.swift
@@ -78,12 +78,9 @@ public struct GithubApi {
     }
     
     func fetchRunStatus(repo:String, in client:Client) async throws -> RunStatus {
-        let uri = URI(string: "\(host)/repos/josercc/sync2gitee/actions/runs?per_page=10")
+        let uri = URI(string: "\(host)/repos/josercc/sync2gitee/actions/runs?per_page=1")
         let response = try await client.get(uri, beforeSend: { request in
             request.headers = headers
-            try request.query.encode([
-                "per_page":"1"
-            ])
         })
         try response.printError(app: app, uri: uri)
         guard  let runResponse = try? response.content.decode(FetchRunStatusResponse.self) else {
